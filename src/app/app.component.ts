98e1bbebf06d7b2a9e298cdc6246e9ec74569038
Stephan Lamoureux <sa.lamoureux@gmail.com>
2022-11-09 21:48:38 -0500

    added transition animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core'
+import { trigger, state, style, animate, transition } from '@angular/animations'
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css'],
+	animations: [
+		trigger('inOutAnimation', [
+			transition(':enter', [
+				style({ opacity: 0 }),
+				animate('0.5s ease-out', style({ opacity: 1 })),
+			]),
+		]),
+	],
 })
 export class AppComponent {
 	newMember: string = ''
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
 import { AppComponent } from './app.component'
 import { TeamComponent } from './team/team.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
 	declarations: [AppComponent, TeamComponent],
-	imports: [BrowserModule],
+	imports: [BrowserModule, BrowserAnimationsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
